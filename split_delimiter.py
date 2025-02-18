7517a0a4fbd486b13b1bf3170de45ff583ecf641
Ikit24 <szszattila2@gmail.com>
2025-02-18 19:13:32 +0200

    added test for the split delimiter

--- split_delimiter.py
+++ split_delimiter.py
@@ -1,12 +1,17 @@
+from textnode import TextNode, TextType
+
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     result_nodes = []
     for old_node in old_nodes:
         if old_node.text_type != TextType.TEXT:
             result_nodes.append(old_node)
         else:
+            delimiter_count = old_node.text.count(delimiter)
+            if delimiter_count % 2 != 0:
+                raise ValueError("Invalid markdown: delimiters must be matched pairs")
             parts = old_node.text.split(delimiter)
             for i in range(len(parts)):
-                if i%2 == 0:
+                if i % 2 == 0:
                     result_nodes.append(TextNode(parts[i], TextType.TEXT))
                 else:
                     result_nodes.append(TextNode(parts[i], text_type))
---
+++ test_split_delimiter.py
@@ -0,0 +1,21 @@
+import unittest
+
+from split_delimiter import split_nodes_delimiter
+from textnode import TextNode, TextType
+
+class TextSplitDelimiter(unittest.TestCase):
+    def test_split_node_delimiter_code(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(
+                nodes,
+                [
+                    TextNode("This is text with a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" word", TextType.TEXT),
+            ]
+        )
+
+if __name__ == "__main__":
+    unittest.main()
+
